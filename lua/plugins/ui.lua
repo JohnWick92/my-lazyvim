b2733533718354933c56d0a2014502a8f8e1f095
JohnWick92 <grumec92@gmail.com>
2024-05-03 11:15:31 -0300

    Changing lualine for nvchad

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,33 @@
+return {
+  { "nvim-lualine/lualine.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "goolord/alpha-nvim", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim" },
+  {
+    "echasnovski/mini.indentscope",
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason", "nvdash", "nvcheatsheet" },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+  },
+  {
+    "kingavatar/nvchad-ui.nvim",
+    branch = "v2.0",
+    lazy = false,
+    config = function()
+      require("nvchad_ui").setup({
+        lazyVim = true,
+        statusline = { separator_style = "default", theme = "default", lualine = false, enabled = true },
+        theme_toggle = { "tokyonight" },
+        nvdash = { load_on_startup = true },
+      })
+
+      -- rename nvchad
+      vim.keymap.set("n", "<leader>cn", require("nvchad_ui.renamer").open, { desc = "nvchad Rename" })
+    end,
+  },
+}
