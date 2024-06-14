6ed54ef8e4eba527f97a8981b5104576c03d976b
JohnWick92 <grumec92@gmail.com>
2024-06-14 17:19:32 -0300

    workin on support to php/laravel

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,8 +1,8 @@
 return {
   {
     "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
+    config = function()
+      require("luasnip.loaders.from_snipmate").lazy_load({ path = "~/.config/nvim/snippets" })
     end,
   },
   {
